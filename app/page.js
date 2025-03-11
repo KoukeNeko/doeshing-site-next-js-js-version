a6c61e798c31fda6f75daeb8b522ae18875ab55c
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-03-11 13:45:58 +0800

    Add email button with hover effect and adjust button sizes for better UI

--- app/page.js
+++ app/page.js
@@ -35,6 +35,7 @@ export default function Home() {
   const [isLinkedinHovered, setIsLinkedinHovered] = useState(false);
   const [isResumeHovered, setIsResumeHovered] = useState(false);
   const [isGithubHovered, setIsGithubHovered] = useState(false);
+  const [isEmailHovered, setIsEmailHovered] = useState(false);
 
   return (
     <div className="w-screen h-screen relative flex">
@@ -44,9 +45,8 @@ export default function Home() {
       </div>
 
       {/* Social Buttons - Floating on right side */}
-      <div className={`absolute ${isMobile ? "top-1/2 right-0 " : "top-1/2 right-1/3"} flex flex-col items-center justify-center z-10`}
-        // 使用 Transform 來調整位置
-        style={{ transform: "translate(50%, -50%)" }}
+      <div className={`absolute ${isMobile ? "top-1/2 right-0" : "top-1/2 right-1/3"} z-10`}
+        style={isMobile ? { transform: "translateY(-50%)" } : { transform: "translate(50%, -50%)" }}
       >
         {!isMobile && <div className="flex flex-col space-y-4 mb-8">
           <TypewriterText
@@ -59,38 +59,50 @@ export default function Home() {
           />
         </div>}
 
-        <div className={`flex ${isMobile ? "flex-col space-y-4" : "space-x-6"}`}>
+        <div className={`flex ${isMobile ? "flex-col space-y-6" : "space-x-6"}`}>
           <Button
-            className={`${isMobile ? "px-4 py-3 w-12 h-12 shadow-lg -translate-x-8" : "mb-4"} cursor-pointer rounded-full transition-all duration-300 hover:scale-110`}
+            className={`${isMobile
+              ? "px-5 py-3 w-14 h-14 shadow-lg "
+              : "mb-4"
+              } cursor-pointer  ${isMobile ? "rounded-r-none" : ""} flex items-center justify-center`}
             href="https://www.linkedin.com/in/doeshing/"
             onMouseEnter={() => setIsLinkedinHovered(true)}
             onMouseLeave={() => setIsLinkedinHovered(false)}
           >
-            <LinkedinIcon size={isMobile ? 28 : 24} />
+            <LinkedinIcon size={isMobile ? 30 : 24} className="text-white" />
           </Button>
           <Button
-            className={`${isMobile ? "px-4 py-3 w-12 h-12 shadow-lg -translate-x-8" : "mb-4"} cursor-pointer rounded-full transition-all duration-300 hover:scale-110`}
+            className={`${isMobile
+              ? "px-5 py-3 w-14 h-14 shadow-lg"
+              : "mb-4"
+              } cursor-pointer  ${isMobile ? "rounded-r-none" : ""} flex items-center justify-center`}
             href="/resume.pdf"
             onMouseEnter={() => setIsResumeHovered(true)}
             onMouseLeave={() => setIsResumeHovered(false)}
           >
-            <FileUser size={isMobile ? 28 : 24} />
+            <FileUser size={isMobile ? 30 : 24} className="text-white" />
           </Button>
           <Button
-            className={`${isMobile ? "px-4 py-3 w-12 h-12 shadow-lg -translate-x-8" : "mb-4"} cursor-pointer rounded-full transition-all duration-300 hover:scale-110`}
+            className={`${isMobile
+              ? "px-5 py-3 w-14 h-14 shadow-lg "
+              : "mb-4"
+              } cursor-pointer  ${isMobile ? "rounded-r-none" : ""} flex items-center justify-center`}
             href=""
             onMouseEnter={() => setIsGithubHovered(true)}
             onMouseLeave={() => setIsGithubHovered(false)}
           >
-            <Github size={isMobile ? 28 : 24} />
+            <Github size={isMobile ? 30 : 24} className="text-white" />
           </Button>
           <Button
-            className={`${isMobile ? "px-4 py-3 w-12 h-12 shadow-lg -translate-x-8" : "mb-4"} cursor-pointer rounded-full transition-all duration-300 hover:scale-110`}
+            className={`${isMobile
+              ? "py-3 w-14 h-14 shadow-lg"
+              : "mb-4"
+              } cursor-pointer  ${isMobile ? "rounded-r-none" : ""} flex items-center justify-center`}
             href="mailto:contact@doeshinf.ink"
             onMouseEnter={() => setIsEmailHovered(true)}
             onMouseLeave={() => setIsEmailHovered(false)}
           >
-            <Mail size={isMobile ? 28 : 24} />
+            <Mail size={isMobile ? 30 : 24} className="text-white" />
           </Button>
         </div>
       </div>
