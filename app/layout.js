4dc282f8df5328d79c6741e14523121a07eed8ed
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-03-12 15:23:36 +0800

    Update RootLayout styles for improved layout and minimum height

--- app/layout.js
+++ app/layout.js
@@ -27,13 +27,11 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      style={{
-        backgroundColor: "#09090b",
-      }}
     >
       <body
         style={{
           backgroundColor: "#09090b",
+          minHeight: "100dvh",
         }}
         className={`${inter.className} ${geistSans.variable} ${geistMono.variable} min-h-dvh flex flex-col bg-zinc-950 text-zinc-200 antialiased`}
       >
